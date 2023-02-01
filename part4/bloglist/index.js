f3e179c5003d00baa87aa0858cf0c7d44c0d2799
0x464e <36742501+0x464e@users.noreply.github.com>
2023-02-01 14:54:41 +0200

    part 4b initial setup

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -6,7 +6,8 @@ const blogsRouter = require("./controllers/blogs");
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
-mongoose.connect(config.MONGODB_URI);
+const MONGO_URI = process.env.NODE_ENV === "test" ? config.MONGODB_URI_TEST : config.MONGODB_URI;
+mongoose.connect(MONGO_URI);
 
 app.use(cors());
 app.use(express.json());
